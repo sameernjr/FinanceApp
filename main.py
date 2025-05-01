2259d68e4a320f88bd68f70390b911b7502e0eee
Sameer <njorogesameer@gmail.com>
2025-05-01 07:34:09 +0300

    created tabs for Debits and Credits

--- main.py
+++ main.py
@@ -10,7 +10,11 @@ st.set_page_config(page_title= "Simple Finance App", page_icon="💰", layout="w
 def load_transactions(file):
     try:
         df = pd.read_csv(file)
+        df.columns = [col.strip() for col in df.columns]
+        df["Amount"] = df["Amount"].str.replace(",", "").astype(float)
+        df["Date"] = pd.to_datetime(df["Date"], format="%d %b %Y")
         st.write(df)
+        return df  # Return the DataFrame after processing
     except Exception as e:
         st.error(f"Error processing file {str(e)}")
         return None
@@ -23,5 +27,15 @@ def main():
 
     if uploaded_file is not None:
         df = load_transactions(uploaded_file)
+
+        if df is not None:
+            debits_df = df[df["Debit/Credit"] == "Debit"].copy()
+            credits_df = df[df["Debit/Credit"] == "Credit"].copy()
+
+            tab1, tab2 = st.tabs(["Expenses (Debits)", "Payments (Credits)"])
+            with tab1:
+                st.write(debits_df)
+            with tab2:
+                st.write(credits_df)  
 
 main()
