e490533d4b89a99e7805aa36d7a272d6ecd36417
Sameer <njorogesameer@gmail.com>
2025-05-01 06:36:40 +0300

    installed all packages

---
+++ main.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+import json
+import os
+
+
+st.set_page_config(page_title= "Simple Finance App", page_icon="💰", layout="wide")
+
+st.write("hello world")
